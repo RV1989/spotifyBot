804fd3d31eed98360c8c058b4f1c2e8b2fb12a73
Rien Verbeke <rien__verbeke@msn.com>
2022-03-30 11:12:58 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const ngrok = require('ngrok');
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const port = 3000
+
+
+
+
+
+    app.post('/', (req, res) => {
+        console.log(req.body)
+        res.send('Hello World!')
+      })
+    
+      app.get('/', (req, res) => {
+        res.send('Hello World!')
+      })
+
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+
+
