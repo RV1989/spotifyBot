bc6c9ce096a4056d3658fe21d6a29a2cc0ca2158
Rien Verbeke <rien__verbeke@msn.com>
2022-03-30 13:23:17 +0200

    test

--- index.js
+++ index.js
@@ -1,25 +1,109 @@
-const ngrok = require('ngrok');
-const express = require('express')
-require('dotenv').config()
-const app = express()
-const port = process.env.PORT
+const ngrok = require("ngrok");
+const express = require("express");
+require("dotenv").config();
+var SpotifyWebApi = require("spotify-web-api-node");
+const app = express();
+const port = process.env.PORT;
 
+const scopes = [
+  "ugc-image-upload",
+  "user-read-playback-state",
+  "user-modify-playback-state",
+  "user-read-currently-playing",
+  "streaming",
+  "app-remote-control",
+  "user-read-email",
+  "user-read-private",
+  "playlist-read-collaborative",
+  "playlist-modify-public",
+  "playlist-read-private",
+  "playlist-modify-private",
+  "user-library-modify",
+  "user-library-read",
+  "user-top-read",
+  "user-read-playback-position",
+  "user-read-recently-played",
+  "user-follow-read",
+  "user-follow-modify",
+];
 
+const spotifyApi = new SpotifyWebApi({
+  redirectUri: `https://spotifyrvb.herokuapp.com//callback`,
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+});
 
 app.use(express.json());
 
-    app.post('/', (req, res) => {
-        console.log(req.body)
-        res.send('Hello World!')
-      })
-    
-      app.get('/', (req, res) => {
-        res.send('Hello World!')
-      })
+app.post("/", (req, res) => {
+  console.log(req.body);
+  spotifyCommand(req.body.plainTextContent);
+  res.send("Hello World!");
+});
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+app.get("/login", (req, res) => {
+  res.redirect(spotifyApi.createAuthorizeURL(scopes));
+});
 
+app.get("/callback", (req, res) => {
+  const error = req.query.error;
+  const code = req.query.code;
+  const state = req.query.state;
 
+  if (error) {
+    console.error("Callback Error:", error);
+    res.send(`Callback Error: ${error}`);
+    return;
+  }
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      const access_token = data.body["access_token"];
+      const refresh_token = data.body["refresh_token"];
+      const expires_in = data.body["expires_in"];
+
+      spotifyApi.setAccessToken(access_token);
+      spotifyApi.setRefreshToken(refresh_token);
+
+      console.log("access_token:", access_token);
+      console.log("refresh_token:", refresh_token);
+
+      console.log(
+        `Sucessfully retreived access token. Expires in ${expires_in} s.`
+      );
+      res.send("Success! You can now close the window.");
+
+      setInterval(async () => {
+        const data = await spotifyApi.refreshAccessToken();
+        const access_token = data.body["access_token"];
+
+        console.log("The access token has been refreshed!");
+        console.log("access_token:", access_token);
+        spotifyApi.setAccessToken(access_token);
+      }, (expires_in / 2) * 1000);
+    })
+    .catch((error) => {
+      console.error("Error getting Tokens:", error);
+      res.send(`Error getting Tokens: ${error}`);
+    });
+});
+
+const spotifyCommand = async (command) => {
+  const {
+    groups: { songToAdd },
+  } = /queue(?<songToAdd>[^ $]*)/i.test(command);
+  if (songToAdd) {
+    songs = await spotifyApi.searchTracks(songToAdd);
+    if (songs) {
+      await spotifyApi.addToQueue(songs.body.tracks.items[0].uri);
+    }
+  }
+};
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
